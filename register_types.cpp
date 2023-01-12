bebcf4fdfe3cc702a9a0fa8a455a1fbcebc3b189
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-12 11:34:20 -0800

    Cleanup _subdivision_server.

--- register_types.cpp
+++ register_types.cpp
@@ -4,10 +4,10 @@
 
 #include "modules/subdiv/src/import/topology_data_importer.hpp"
 #include "modules/subdiv/src/resources/baked_subdiv_mesh.hpp"
-#include "src/resources/topology_data_mesh.hpp"
 #include "modules/subdiv/src/subdivision/subdivision_baker.hpp"
 #include "modules/subdiv/src/subdivision/subdivision_mesh.hpp"
 #include "modules/subdiv/src/subdivision/subdivision_server.hpp"
+#include "src/resources/topology_data_mesh.hpp"
 
 #include "modules/subdiv/src/subdivision/quad_subdivider.hpp"
 #include "modules/subdiv/src/subdivision/subdivider.hpp"
@@ -20,7 +20,6 @@
 static SubdivisionServer *_subdivision_server;
 
 void initialize_subdiv_module(ModuleInitializationLevel p_level) {
-
 	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
 		ClassDB::register_class<Subdivider>();
 		ClassDB::register_class<QuadSubdivider>();
@@ -41,7 +40,10 @@ void initialize_subdiv_module(ModuleInitializationLevel p_level) {
 }
 
 void uninitialize_subdiv_module(ModuleInitializationLevel p_level) {
-  if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		Engine::get_singleton()->remove_singleton("SubdivisionServer");
-  }
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		if (Engine::get_singleton()->has_singleton("SubdivisionServer") && Engine::get_singleton()->get_singleton_object("SubdivisionServer") == _subdivision_server) {
+			Engine::get_singleton()->remove_singleton("SubdivisionServer");
+			memdelete(_subdivision_server);
+		}
+	}
 }
