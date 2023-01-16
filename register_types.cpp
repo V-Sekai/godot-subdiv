38103f346e45c6aae86a0763a6b247e4329f18dc
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-16 14:45:15 -0800

    Code typo.

--- register_types.cpp
+++ register_types.cpp
@@ -4,10 +4,10 @@
 
 #include "src/import/topology_data_importer.hpp"
 #include "src/resources/baked_subdiv_mesh.hpp"
+#include "src/resources/topology_data_mesh.hpp"
 #include "src/subdivision/subdivision_baker.hpp"
 #include "src/subdivision/subdivision_mesh.hpp"
 #include "src/subdivision/subdivision_server.hpp"
-#include "src/resources/topology_data_mesh.hpp"
 
 #include "src/subdivision/quad_subdivider.hpp"
 #include "src/subdivision/subdivider.hpp"
@@ -20,30 +20,32 @@
 static SubdivisionServer *_subdivision_server;
 
 void initialize_subdiv_module(ModuleInitializationLevel p_level) {
-	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
-		ClassDB::register_class<Subdivider>();
-		ClassDB::register_class<QuadSubdivider>();
-		ClassDB::register_class<TriangleSubdivider>();
-
-		ClassDB::register_class<SubdivisionServer>();
-		ClassDB::register_class<SubdivisionMesh>();
-		ClassDB::register_class<SubdivisionBaker>();
-
-		ClassDB::register_class<TopologyDataImporter>();
-
-		ClassDB::register_class<TopologyDataMesh>();
-		ClassDB::register_class<BakedSubdivMesh>();
-
-		_subdivision_server = memnew(SubdivisionServer);
-		Engine::get_singleton()->add_singleton(Engine::Singleton("SubdivisionServer", _subdivision_server));
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
 	}
+	ClassDB::register_class<Subdivider>();
+	ClassDB::register_class<QuadSubdivider>();
+	ClassDB::register_class<TriangleSubdivider>();
+
+	ClassDB::register_class<SubdivisionServer>();
+	ClassDB::register_class<SubdivisionMesh>();
+	ClassDB::register_class<SubdivisionBaker>();
+
+	ClassDB::register_class<TopologyDataImporter>();
+
+	ClassDB::register_class<TopologyDataMesh>();
+	ClassDB::register_class<BakedSubdivMesh>();
+
+	_subdivision_server = memnew(SubdivisionServer);
+	Engine::get_singleton()->add_singleton(Engine::Singleton("SubdivisionServer", _subdivision_server));
 }
 
 void uninitialize_subdiv_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		if (Engine::get_singleton()->has_singleton("SubdivisionServer") && Engine::get_singleton()->get_singleton_object("SubdivisionServer") == _subdivision_server) {
-			Engine::get_singleton()->remove_singleton("SubdivisionServer");
-			memdelete(_subdivision_server);
-		}
+		return;
+	}
+	if (Engine::get_singleton()->has_singleton("SubdivisionServer") && Engine::get_singleton()->get_singleton_object("SubdivisionServer") == _subdivision_server) {
+		Engine::get_singleton()->remove_singleton("SubdivisionServer");
+		memdelete(_subdivision_server);
 	}
 }
