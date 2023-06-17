5924359e63544deba5aaad437c2744da81cfaa2a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-17 15:17:59 -0700

    Add subdiv plugin ON BY DEFAULT.

--- editor/subdiv_plugin.h
+++ editor/subdiv_plugin.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include "core/error/error_list.h"
 #include "editor/editor_plugin.h"
 #include "editor/import/editor_import_plugin.h"
+#include "modules/gltf/extensions/gltf_document_extension.h"
 #include "modules/gltf/gltf_document.h"
 
+#include "scene/3d/importer_mesh_instance_3d.h"
 #include "subdiv_converter.h"
 
 class GodotSubdivImporter;
@@ -44,4 +47,16 @@ public:
 	Error import(const String &p_source_file, const String &p_save_path, const HashMap<StringName, Variant> &p_options, List<String> *r_platform_variants, List<String> *r_gen_files, Variant *r_metadata = nullptr) override;
 
 	GodotSubdivImporter();
+};
+
+class GLTFDocumentExtensionSubdiv : public GLTFDocumentExtension {
+public:
+	virtual Error import_node(Ref<GLTFState> p_state, Ref<GLTFNode> p_gltf_node, Dictionary &r_json, Node *p_node) {
+		Ref<SubdivConverter> subdiv_converter = Ref<SubdivConverter>(memnew(SubdivConverter(TopologyDataImporter::ImportMode::IMPORTER_MESH, 1)));
+		if (Object::cast_to<ImporterMeshInstance3D>(p_node)) {
+			subdiv_converter->convert_importer_mesh_instances_recursively(p_node);
+			return OK;
+		}
+		return OK;
+	}
 };
